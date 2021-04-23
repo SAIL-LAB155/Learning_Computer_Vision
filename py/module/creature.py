e6da762a7efe2a59418547ab89485e9b9c55a499
SAIL-LAB155 <saillab155@gmail.com>
2021-04-23 17:39:19 +0800

    add test sample of py2qt

--- py/module/creature.py
+++ py/module/creature.py
@@ -1,16 +1,26 @@
+from apis import *
+import cv2
+
 
 class Creature:
     def __init__(self):
         print('Creature init ')
 
-    def train(self, epochs, img_path, model_path=None):
-        pass
+    def train(self, img_path, model_path=None, epochs=10):
+        for epoch in range(int(epochs)):
+            time.sleep(1)
+            message = "{}: {}".format(time.asctime(time.localtime(time.time())), epoch)
+            print(message)
+            SendToQt_Log(message)
 
     def plot(self):
         pass
 
-    def visualize(self, img_path, model_path=None):
-        pass
+    def visualize(self, img_path, model_path):
+        print("Image is currently showing!")
+        img = cv2.imread(img_path[:-1])
+        img = cv2.flip(img, 1)
+        SendToQt_Update_Display(img)
 
     def visualize_webcam(self, model_path=None):
         pass
