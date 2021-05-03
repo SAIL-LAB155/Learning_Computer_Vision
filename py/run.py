5144882a99d42013e41837d01bce8fb0e70c6ce3
CheungBH <saillab155@gmail.com>
2021-05-03 16:12:24 +0800

    cmd with shm

--- py/run.py
+++ py/run.py
@@ -1,11 +1,12 @@
 from apis import *
 from Module_init import Module
 from Parse_signal import SignalParser
-
+import os
 
 
 if __name__ == '__main__':
     parse = SignalParser()
+    os.system("gcc -o libc_send_pic.so -shared -fPIC shm_send_pic.c")
     Comm_Init()
     while True:
         comm_handler()
