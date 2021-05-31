37429bda09ac1526b902177dd3a2edb55c0b5a1a
sean <sean0901@hku.hk>
2021-05-31 18:30:42 +0800

    add visualize

--- py/module/cat.py
+++ py/module/cat.py
@@ -1,6 +1,6 @@
 import os
 from jetson_inference.python.training.classification.train import main
-# from jetson_inference.python.examples.imagenet import main_img
+from jetson_inference.python.examples.imagenet import main_img
 from apis import *
 import cv2
 
@@ -23,23 +23,26 @@ class CAT:
     def plot(self):
         print('Cat dog plot start')
 
-    def visualize(self,img_path, model_path):
+    def visualize(self,img_path, model_path, label_path=None):
+        if not label_path:
+            label_path = self.label_path
         print('img_path', img_path)
         print('model_path:', model_path)
         print('Cat dog vis start')
         # main_img(model_path)
         # cmd = 'python3 imagenet.py --model={} --input_blob=input_0 --output_blob=output_0 --labels={} {} output.jpg'.format(model_path,self.label_path, img_path)
-        cmd = 'python3 /home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/examples/imagenet.py --model={} --input_blob=input_0 --output_blob=output_0 --labels=/home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/training/classification/data/cat_dog/labels.txt {} output.jpg'.format(
-            model_path[:-1], img_path[:-1])
-        print(cmd)
-        os.system(cmd)
-        img = cv2.imread('/home/nvidia/Desktop/Learning_Computer_Vision/py/output.jpg')
-        SendToQt_Update_Display(img)
+        # cmd = 'python3 /home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/examples/imagenet.py --model={} --input_blob=input_0 --output_blob=output_0 --labels=/home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/training/classification/data/cat_dog/labels.txt {} output.jpg'.format(
+        #     model_path[:-1], img_path[:-1])
+        # print(cmd)
+        # os.system(cmd)
+        # img = cv2.imread('/home/nvidia/Desktop/Learning_Computer_Vision/py/output.jpg')
+        main_img(model_path[:-1],img_path[:-1], label_path)
+        # SendToQt_Update_Display(img)
         # 'python3 imagenet.py --model=../training/classification/models/cat_dog/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=../training/classification/data/cat_dog/labels.txt ../training/classification/data/cat_dog/test/cat/01.jpg catttt.jpg^'
 
     def visualize_webcam(self, model_path=None):
-        print('model_path:', model_path)
-        print('Cat dog web_vis start')
+        cmd = 'python3 /home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/examples/imagenet.py /dev/video0 --model={} --input_blob=input_0 --output_blob=output_0 --labels=/home/nvidia/Desktop/Learning_Computer_Vision/py/jetson_inference/python/training/classification/data/cat_dog/labels.txt output.jpg'.format(
+            model_path[:-1])
 
     def annotate(self):
         print('Cat dog training start')
--- rLearning_Computer_Vision/moc_Learn_Computer_Vision_Dialog.cpp
+++ rLearning_Computer_Vision/moc_Learn_Computer_Vision_Dialog.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Qt_py_demo/Qt/Learn_Computer_Vision_Dialog.h"
+#include "Learn_Computer_Vision_Dialog.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
