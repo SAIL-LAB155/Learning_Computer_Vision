58009f0182748bb4f2f1723b0d794821e4e628e0
carolchenyx <carolchenyx@gmail.com>
2021-06-24 16:49:39 +0800

    added lines and excel

--- py/tasks/human_pose/detect_video_final.py
+++ py/tasks/human_pose/detect_video_final.py
@@ -14,6 +14,7 @@ import trt_pose.models
 from torch2trt import TRTModule
 from trt_pose.draw_objects import DrawObjects
 from trt_pose.parse_objects import ParseObjects
+from openpyxl import Workbook
 
 from apis import *
 
@@ -148,19 +149,236 @@ class human_detection:
         image.sub_(mean[:, None, None]).div_(std[:, None, None])
         return image[None, ...]
 
-    def visualize(self, img, src, t):
+    def visualize(self, img, src, t, width, height, frameNumber, sheet):
         color = (0, 255, 0)
         data = self.preprocess(img)
         cmap, paf = self.model_trt(data)
         cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
         counts, objects, peaks = self.parse_objects(cmap, paf)  # , cmap_threshold=0.keypoints0+
         # 15, link_threshold=0.15)
-        X_compress = 1920.0 / self.WIDTH * 1.0
-        Y_compress = 1080.0 / self.HEIGHT * 1.0
+        X_compress = width / self.WIDTH * 1.0
+        Y_compress = height / self.HEIGHT * 1.0
         fps = 1.0 / (time.time() - t)
         
         for i in range(counts[0]):
             keypoints = self.get_keypoint(objects, i, peaks)
+
+            if keypoints[0]:
+                sheet["B" + str(frameNumber)] = keypoints[0][1]
+                sheet["C" + str(frameNumber)] = keypoints[0][2]
+            
+            if keypoints[1]:
+                sheet["D" + str(frameNumber)] = keypoints[1][1]
+                sheet["E" + str(frameNumber)] = keypoints[1][2]
+            
+            if keypoints[2]:
+                sheet["F" + str(frameNumber)] = keypoints[2][1]
+                sheet["G" + str(frameNumber)] = keypoints[2][2]
+            
+            if keypoints[3]:
+                sheet["H" + str(frameNumber)] = keypoints[3][1]
+                sheet["I" + str(frameNumber)] = keypoints[3][2]
+            
+            if keypoints[4]:
+                sheet["J" + str(frameNumber)] = keypoints[4][1]
+                sheet["K" + str(frameNumber)] = keypoints[4][2]
+            
+            if keypoints[5]:
+                sheet["L" + str(frameNumber)] = keypoints[5][1]
+                sheet["M" + str(frameNumber)] = keypoints[5][2]
+            
+            if keypoints[6]:
+                sheet["N" + str(frameNumber)] = keypoints[6][1]
+                sheet["O" + str(frameNumber)] = keypoints[6][2]
+            
+            if keypoints[7]:
+                sheet["P" + str(frameNumber)] = keypoints[7][1]
+                sheet["Q" + str(frameNumber)] = keypoints[7][2]
+            
+            if keypoints[8]:
+                sheet["R" + str(frameNumber)] = keypoints[8][1]
+                sheet["S" + str(frameNumber)] = keypoints[8][2]
+            
+            if keypoints[9]:
+                sheet["T" + str(frameNumber)] = keypoints[9][1]
+                sheet["U" + str(frameNumber)] = keypoints[9][2]
+            
+            if keypoints[10]:
+                sheet["V" + str(frameNumber)] = keypoints[10][1]
+                sheet["W" + str(frameNumber)] = keypoints[10][2]
+            
+            if keypoints[11]:
+                sheet["X" + str(frameNumber)] = keypoints[11][1]
+                sheet["Y" + str(frameNumber)] = keypoints[11][2]
+            
+            if keypoints[12]:
+                sheet["Z" + str(frameNumber)] = keypoints[12][1]
+                sheet["AA" + str(frameNumber)] = keypoints[12][2]
+            
+            if keypoints[13]:
+                sheet["AB" + str(frameNumber)] = keypoints[13][1]
+                sheet["AC" + str(frameNumber)] = keypoints[13][2]
+            
+            if keypoints[14]:
+                sheet["AD" + str(frameNumber)] = keypoints[14][1]
+                sheet["AE" + str(frameNumber)] = keypoints[14][2]
+            
+            if keypoints[15]:
+                sheet["AF" + str(frameNumber)] = keypoints[15][1]
+                sheet["AG" + str(frameNumber)] = keypoints[15][2]
+            
+            if keypoints[16]:
+                sheet["AH" + str(frameNumber)] = keypoints[16][1]
+                sheet["AI" + str(frameNumber)] = keypoints[16][2]
+            
+            if keypoints[17]:
+                sheet["AJ" + str(frameNumber)] = keypoints[17][1]
+                sheet["AK" + str(frameNumber)] = keypoints[17][2]
+
+            if keypoints[6][1] and keypoints[8][1]:
+                x1 = round(keypoints[6][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[6][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[8][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[8][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[8][1] and keypoints[10][1]:
+                x1 = round(keypoints[8][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[8][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[10][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[10][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[5][1] and keypoints[7][1]:
+                x1 = round(keypoints[5][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[5][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[7][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[7][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[7][1] and keypoints[9][1]:
+                x1 = round(keypoints[7][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[7][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[9][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[9][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[6][1] and keypoints[17][1]:
+                x1 = round(keypoints[6][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[6][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[17][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[17][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[5][1] and keypoints[17][1]:
+                x1 = round(keypoints[5][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[5][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[17][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[17][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[17][1] and keypoints[12][1]:
+                x1 = round(keypoints[17][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[17][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[12][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[12][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[17][1] and keypoints[11][1]:
+                x1 = round(keypoints[17][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[17][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[11][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[11][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[12][1] and keypoints[14][1]:
+                x1 = round(keypoints[12][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[12][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[14][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[14][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[14][1] and keypoints[16][1]:
+                x1 = round(keypoints[14][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[14][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[16][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[16][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[11][1] and keypoints[13][1]:
+                x1 = round(keypoints[11][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[11][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[13][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[13][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
+            if keypoints[13][1] and keypoints[15][1]:
+                x1 = round(keypoints[13][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[13][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[15][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[15][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[17][1] and keypoints[0][1]:
+                x1 = round(keypoints[17][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[17][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[0][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[0][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[4][1] and keypoints[2][1]:
+                x1 = round(keypoints[4][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[4][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[2][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[2][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[3][1] and keypoints[1][1]:
+                x1 = round(keypoints[3][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[3][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[1][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[1][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[0][1] and keypoints[2][1]:
+                x1 = round(keypoints[0][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[0][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[2][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[2][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[0][1] and keypoints[1][1]:
+                x1 = round(keypoints[0][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[0][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[1][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[1][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+            
+            if keypoints[1][1] and keypoints[2][1]:
+                x1 = round(keypoints[1][2] * self.WIDTH * X_compress)
+                y1 = round(keypoints[1][1] * self.HEIGHT * Y_compress)
+
+                x2 = round(keypoints[2][2] * self.WIDTH * X_compress)
+                y2 = round(keypoints[2][1] * self.HEIGHT * Y_compress)
+                cv2.line(src, (x1, y1), (x2, y2), color, 2)
+
             for j in range(len(keypoints)):
                 if keypoints[j][1]:
                     x = round(keypoints[j][2] * self.WIDTH * X_compress)
@@ -223,25 +441,34 @@ class human_detection:
         else:
             return 0
 
-    def run_video(self, video):
+    def run_video(self, video, sheet):
         source_path = video
         cap = cv2.VideoCapture(source_path)
+
+        width  = 1920
+        height = 1080
+
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        out_video = cv2.VideoWriter("test.mp4", fourcc, cap.get(cv2.CAP_PROP_FPS), (1920, 1080))
+        out_video = cv2.VideoWriter("../videos/" + video.split('/')[-1], fourcc, cap.get(cv2.CAP_PROP_FPS), (width, height))
         if cap is None:
             print("Camera Open Error")
             sys.exit(0)
 
+        frameNumber = 1
+
         while cap.isOpened():
+            frameNumber = frameNumber + 1
+            sheet["A" + str(frameNumber)] = frameNumber - 1
             t = time.time()
             ret_val, dst = cap.read()
             if ret_val == False:
                 print("Camera read Error")
                 break
             img = cv2.resize(dst, dsize=(self.WIDTH, self.HEIGHT), interpolation=cv2.INTER_AREA)
-            img = self.visualize(img, dst, t)
+            img = self.visualize(img, dst, t, width, height, frameNumber, sheet)
             cv2.imshow("result", img)
             out_video.write(img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -278,8 +505,48 @@ class human_detection:
 
 
 def detect_video(video):
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet["A1"] = "frame/keypoints"
+    sheet["B1"] = "0x"
+    sheet["C1"] = "0y"
+    sheet["D1"] = "1x"
+    sheet["E1"] = "1y"
+    sheet["F1"] = "2x"
+    sheet["G1"] = "2y"
+    sheet["H1"] = "3x"
+    sheet["I1"] = "3y"
+    sheet["J1"] = "4x"
+    sheet["K1"] = "4y"
+    sheet["L1"] = "5x"
+    sheet["M1"] = "5y"
+    sheet["N1"] = "6x"
+    sheet["O1"] = "6y"
+    sheet["P1"] = "7x"
+    sheet["Q1"] = "7y"
+    sheet["R1"] = "8x"
+    sheet["S1"] = "8y"
+    sheet["T1"] = "9x"
+    sheet["U1"] = "9y"
+    sheet["V1"] = "10x"
+    sheet["W1"] = "10y"
+    sheet["X1"] = "11x"
+    sheet["Y1"] = "11y"
+    sheet["Z1"] = "12x"
+    sheet["AA1"] = "12y"
+    sheet["AB1"] = "13x"
+    sheet["AC1"] = "13y"
+    sheet["AD1"] = "14x"
+    sheet["AE1"] = "14y"
+    sheet["AF1"] = "15x"
+    sheet["AG1"] = "15y"
+    sheet["AH1"] = "16x"
+    sheet["AI1"] = "16y"
+    sheet["AJ1"] = "17x"
+    sheet["AK1"] = "17y"
     human = human_detection()
-    human.run_video(video)
+    human.run_video(video, sheet)
+    workbook.save(filename="../excel/" + video.split('/')[-1] + ".xlsx")
 
 def detect_image(image):
     human = human_detection()
